ed8dfacb47be4110b27db0c2e7c39a1b039d89b6
MichelleMaluks <chellemaluleke@gmail.com>
2026-06-25 00:20:15 +0200

    cors edit

--- config/cors.php
+++ config/cors.php
@@ -3,10 +3,13 @@
 return [
     'paths' => ['api/*'],   // only apply to API routes
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'], // React dev server
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://agrico-k7cw.onrender.com'
+    ], // React dev server
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 ];
