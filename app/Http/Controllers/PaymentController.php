62ea65b144fa08eca1eff35099a02bf09907351b
MichelleMaluks <chellemaluleke@gmail.com>
2026-07-01 03:27:43 +0200

    payfast add log

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -42,6 +42,10 @@ class PaymentController extends Controller
         $signatureString = collect($data)
             ->map(fn($v, $k) => $k . '=' . rawurlencode($v))
             ->implode('&');
+
+        \Log::info('PayFast redirect data', $data);
+        \Log::info('Signature string', ['string' => $signatureString]);
+        \Log::info('Generated signature', ['sig' => md5($signatureString)]);
         $data['signature'] = md5($signatureString);
 
         return view('payfast.redirect', compact('payfastUrl', 'data'));
@@ -86,6 +90,11 @@ class PaymentController extends Controller
 
         $generatedSignature = md5($signatureString . trim(config('services.payfast.merchant_key')));
 
+
+        \Log::info('IPN data received', $data);
+        \Log::info('IPN signature string', ['string' => $signatureString]);
+        \Log::info('Generated IPN signature', ['sig' => $generatedSignature]);
+        \Log::info('Submitted signature', ['sig' => $request->signature]);
         if ($generatedSignature !== $request->signature) {
             return response('Invalid signature', 400);
         }
